9b958a97478614228d2358ed76e1db89eae81d0f
Anthony <bousek.antonin@gmail.com>
2016-04-11 16:50:12 +0200

    Initialization dropdown for better tweaking

--- js/imhere.js
+++ js/imhere.js
@@ -1,18 +1,29 @@
  // as the page loads, cal these scripts
-(function($){
-  $(function(){
+ (function($) {
+     $(function() {
 
 
-// SIDE MENU
-  $('.button-collapse').sideNav({
-      menuWidth: 300, // Default is 240
-      edge: 'right', // Choose the horizontal origin
-      closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
-    }
-  );
-      
-  	
-$('li.page_item').addClass('waves-effect waves-light');
+         // SIDE MENU
+         $('.button-collapse').sideNav({
+             menuWidth: 300, // Default is 240
+             edge: 'right', // Choose the horizontal origin
+             closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+         });
 
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
+         //waves efffect on NAV
+         $('li.page_item').addClass('waves-effect waves-light');
+
+         // dropdown initialization
+         $('.dropdown-button').dropdown({
+             inDuration: 300,
+             outDuration: 225,
+             constrain_width: false, // Does not change width of dropdown to that of the activator
+             hover: true, // Activate on hover
+             gutter: 0, // Spacing from edge
+             belowOrigin: false, // Displays dropdown below the button
+             alignment: 'left' // Displays dropdown with edge aligned to the left of button
+         });
+
+
+     }); // end of document ready
+ })(jQuery); // end of jQuery name space
