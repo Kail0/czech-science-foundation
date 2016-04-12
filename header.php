930de341f2a628b51f12a80ebdcdea4e4d04a17c
Anthony <bousek.antonin@gmail.com>
2016-04-12 16:35:45 +0200

    better readability of code

--- header.php
+++ header.php
@@ -46,9 +46,18 @@
 					<nav id="site-navigation" class="main-navigation" role="navigation">
 						<a href="#" data-activates="mobile-nav" class="button-collapse"><i class="material-icons">menu</i></a>
 
-						<?php wp_nav_menu( array( 'theme_location' => 'drawer', 'menu_class' => 'menu side-nav', 'menu_id' => 'mobile-nav','items_wrap' => '<ul id="%1$s" class="%2$s"><li class="mobile-header"><p>Menu</p></li>%3$s</ul><div class="clear"></div>', ) ); ?>
+						<?php wp_nav_menu( array(
+							'theme_location' => 'drawer',
+							'menu_class' => 'menu side-nav',
+							'menu_id' => 'mobile-nav',
+							'items_wrap' => '<ul id="%1$s" class="%2$s"><li class="mobile-header"><p>Menu</p></li>%3$s</ul><div class="clear"></div>'
+							)); ?>
 
-						<?php wp_nav_menu( array('theme_location'=>'primary', 'menu_class' => 'hide-on-med-and-down', 'walker' => new Materialize_CSS_Menu_Walker(),'depth' => 0) ); ?>
+						<?php wp_nav_menu( array(
+							'theme_location'=>'primary',
+							'menu_class' => 'hide-on-med-and-down',
+							'walker' => new Materialize_CSS_Menu_Walker()
+							)); ?>
 
 					</nav><!-- #site-navigation -->
 				<div class="clear"></div>
