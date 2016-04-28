9f3311edf6ae581c82ea26f6a7bf76a9f9c164c5
Anthony <bousek.antonin@gmail.com>
2016-04-28 09:56:51 +0200

    card support for other pages

--- page.php
+++ page.php
@@ -14,25 +14,32 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+<div class="section">
+    <div class="row">
+        <div class="col s12 m8 l9">
+            <div id="primary" class="content-area">
+                <main id="main" class="site-main" role="main">
+                    <?php
+                        while ( have_posts() ) : the_post();
 
-			<?php
-			while ( have_posts() ) : the_post();
+                            get_template_part( 'template-parts/content', 'page' );
 
-				get_template_part( 'template-parts/content', 'page' );
+                            // If comments are open or we have at least one comment, load up the comment template.
+                            if ( comments_open() || get_comments_number() ) :
+                                comments_template();
+                            endif;
 
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
+                        endwhile; // End of the loop.
+                        ?>
+                </main><!-- #main -->
+            </div><!-- #primary -->
+        </div><!-- col -->
+        <aside id="secondary" class="widget-area col s12 m4 l3" role="complementary">
+            <?php dynamic_sidebar( 'page' ); ?>
+        </aside><!-- #secondary -->
+    </div><!-- row -->
+</div><!-- section -->
 
-			endwhile; // End of the loop.
-			?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
 
 <?php
-get_sidebar();
 get_footer();
