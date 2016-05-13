cb6d74b29a2c4e2f89fafafb1b9bb84b4b84a41a
Anthony <bousek.antonin@gmail.com>
2016-05-13 14:16:13 +0200

    Added transition support for blog.
    If its single post, there is no animation yet

--- template-parts/content.php
+++ template-parts/content.php
@@ -12,7 +12,7 @@
 <?php if ( is_single() ) { ?>
 
 	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-		<div class="card dismissable">
+		<div class="card">
 		<?php if (has_post_thumbnail()) { ?>
 			<div class="card-image">
 				<?php the_post_thumbnail('card-header');?>
@@ -81,51 +81,53 @@
 
 <?php } else { ?>
 
+	<div data-animation="animate">
 	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-		<div class="card dismissable">
-		    <div class="card-content">
-		        <span class="card-title">
-						<header class="entry-header CONTENT">
-							<?php
-								//if is single post add H1, otherwise add H2
-								if ( is_single() ) {
-									the_title( '<h1 class="entry-title">', '</h1>' );
-								} else {
-									the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-								}
+			<div class="card">
+			    <div class="card-content">
+			        <span class="card-title">
+							<header class="entry-header CONTENT">
+								<?php
+									//if is single post add H1, otherwise add H2
+									if ( is_single() ) {
+										the_title( '<h1 class="entry-title">', '</h1>' );
+									} else {
+										the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+									}
 
-							if ( 'post' === get_post_type() ) : ?>
-							<div class="entry-meta">
-								<?php gacr_posted_on(); ?>
-							</div><!-- .entry-meta -->
-							<?php
-							endif; ?>
-						</header><!-- .entry-header -->
-		            </span>
-			<div class="entry-content">
-				<?php
-					the_content( sprintf(
-						/* translators: %s: Name of current post. */
-						wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'gacr' ), array( 'span' => array( 'class' => array() ) ) ),
-						the_title( '<span class="screen-reader-text">"', '"</span>', false )
-					) );
+								if ( 'post' === get_post_type() ) : ?>
+								<div class="entry-meta">
+									<?php gacr_posted_on(); ?>
+								</div><!-- .entry-meta -->
+								<?php
+								endif; ?>
+							</header><!-- .entry-header -->
+			            </span>
+				<div class="entry-content">
+					<?php
+						the_content( sprintf(
+							/* translators: %s: Name of current post. */
+							wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'gacr' ), array( 'span' => array( 'class' => array() ) ) ),
+							the_title( '<span class="screen-reader-text">"', '"</span>', false )
+						) );
 
-					wp_link_pages( array(
-						'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'gacr' ),
-						'after'  => '</div>',
-					) );
-				?>
-			</div><!-- .entry-content -->
-			</div> <!-- card content -->
-			<div class="card-action">
-				<footer class="entry-footer">
-					<a href="<?php esc_url( the_permalink() ); ?>" class="white btn-floating waves-effect waves-circle z-depth-0">
-					    <?php printf( esc_html( '%s', 'gacr' ), '<i class="material-icons gacr-blue-text">&#xE5C8;</i>' ); ?>
-					</a>
-				</footer><!-- .entry-footer -->
-			</div>
+						wp_link_pages( array(
+							'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'gacr' ),
+							'after'  => '</div>',
+						) );
+					?>
+				</div><!-- .entry-content -->
+				</div> <!-- card content -->
+				<div class="card-action">
+					<footer class="entry-footer">
+						<a href="<?php esc_url( the_permalink() ); ?>" class="white btn-floating waves-effect waves-circle z-depth-0">
+						    <?php printf( esc_html( '%s', 'gacr' ), '<i class="material-icons gacr-blue-text">&#xE5C8;</i>' ); ?>
+						</a>
+					</footer><!-- .entry-footer -->
+				</div>
 
-		</div><!-- card -->
-	</article><!-- #post-## -->
+			</div><!-- card -->
+		</article><!-- #post-## -->
+		</div> <!-- data attrib -->
 
 <?php } ?>
