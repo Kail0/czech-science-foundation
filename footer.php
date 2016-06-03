c07121348f1aa2c876f84c17299b961aef41e2c2
Anthony <bousek.antonin@gmail.com>
2016-06-03 17:12:07 +0200

    Divs are running..

--- footer.php
+++ footer.php
@@ -10,10 +10,10 @@
  */
 
 ?>
-<div class="clear"></div>
-</div><!-- .container -->
-	</div><!-- #content -->
-
+        <div class="clear"></div>
+        </div>
+    </div><!-- .container -->
+</div><!-- #content -->
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'gacr' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'gacr' ), 'WordPress' ); ?></a>
