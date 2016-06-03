5f806a3cfb185de713fc5eeaad74544ea9d605c1
Anthony <bousek.antonin@gmail.com>
2016-06-03 17:14:44 +0200

    I fucked up one if statement
    now its correct

--- template-parts/content.php
+++ template-parts/content.php
@@ -9,12 +9,12 @@
 
 ?>
 <!-- If is post page single, show pictures of header, if not, show regular card with not images -->
-<?php if ( !is_single() && is_front_page() && !wp_is_mobile() ) { ?>
+<?php if ( !is_single() && is_front_page() && !wp_is_mobile() ) : ?>
         <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
             <div class="card">
                 <div class="card-content">
                     <span class="card-title">
-                            <header class="entry-header CONTENT">
+                            <header class="entry-header">
                                 <?php
                                     //if is single post add H1, otherwise add H2
                                     if ( is_single() ) {
@@ -56,7 +56,7 @@
             </div><!-- card -->
         </article><!-- #post-## -->
 
-<?php } elseif ( is_single() ) { ?>
+<?php elseif ( is_single() ) : ?>
 
     <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
         <div class="card">
@@ -126,7 +126,7 @@
         </div><!--  card -->
     </article><!-- #post-## -->
 
-<?php } elseif ( is_front_page() && wp_is_mobile() ) { ?>
+<?php elseif ( is_front_page() && wp_is_mobile() ) : ?>
 
 <div data-animation="animate">
     <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
@@ -176,4 +176,53 @@
             </div><!-- card -->
         </article><!-- #post-## -->
  </div>
-<?php } ?>
+<?php else: ?>
+            <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+            <div class="card">
+                <div class="card-content">
+                    <span class="card-title">
+                            <header class="entry-header">
+                                <?php
+                                    //if is single post add H1, otherwise add H2
+                                    if ( is_single() ) {
+                                        the_title( '<h1 class="entry-title">', '</h1>' );
+                                    } else {
+                                        the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+                                    }
+
+                                if ( 'post' === get_post_type() ) : ?>
+                                <div class="entry-meta">
+                                    <?php gacr_posted_on(); ?>
+                                </div><!-- .entry-meta -->
+                                <?php
+                                endif; ?>
+                            </header><!-- .entry-header -->
+                        </span>
+                <div class="entry-content">
+                    <?php
+                        the_content( sprintf(
+                            /* translators: %s: Name of current post. */
+                            wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'gacr' ), array( 'span' => array( 'class' => array() ) ) ),
+                            the_title( '<span class="screen-reader-text">"', '"</span>', false )
+                        ) );
+
+                        wp_link_pages( array(
+                            'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'gacr' ),
+                            'after'  => '</div>',
+                        ) );
+                    ?>
+                </div><!-- .entry-content -->
+                </div> <!-- card content -->
+                <div class="card-action">
+                    <footer class="entry-footer">
+                        <a href="<?php esc_url( the_permalink() ); ?>" class="white btn-floating waves-effect waves-circle z-depth-0">
+                            <?php printf( esc_html( '%s', 'gacr' ), '<i class="material-icons gacr-blue-text">&#xE5C8;</i>' ); ?>
+                        </a>
+                    </footer><!-- .entry-footer -->
+                </div>
+            </div><!-- card -->
+        </article><!-- #post-## -->
+
+
+
+<?php endif; ?>
