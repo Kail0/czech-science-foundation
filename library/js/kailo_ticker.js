2f0f3e101c2d1a665d8d4e7b4d79ae9d1ef1adcd
Anthony <bousek.antonin@gmail.com>
2016-08-22 14:36:51 +0200

    rewriten Ticker plugin from scratch

--- library/js/kailo_ticker.js
+++ library/js/kailo_ticker.js
@@ -1,52 +1,219 @@
+//Javascript codes
 
-!function() {
-    jQuery.fn.eventTicker = function(l) {
-        var n = {width: "100%",modul: "eventTicker",color: "default",border: !1,effect: "fade",fontstyle: "normal",autoplay: !1,timer: 4e3,feed: !1,feedlabels: !1,feedcount: 5}, e = [], d = [], l = jQuery.extend(n, l);
-        return this.each(function() {
-            function n() {
-                l.modul.width() < 480 ? (l.modul.find(".bn-title h2").css({display: "none"}), l.modul.find(".bn-title").css({width: 10}), l.modul.find("ul").css({left: 30})) : (l.modul.find(".bn-title h2").css({display: "inline-block"}), l.modul.find(".bn-title").css({width: "auto"}), l.modul.find("ul").css({left: jQuery(l.modul).find(".bn-title").width() + 30}))
-            }
-            function i() {
-                u++, u == s && (u = 0), o()
-            }
-            function o() {
-                "fade" == l.effect ? (l.modul.find("ul li").css({display: "none"}), l.modul.find("ul li").eq(u).fadeIn("normal", function() {
-                    c = !0
-                })) : "slide-h" == l.effect ? l.modul.find("ul li").eq(f).animate({width: 0}, function() {
-                    jQuery(this).css({display: "none",width: "100%"}), l.modul.find("ul li").eq(u).css({width: 0,display: "block"}), l.modul.find("ul li").eq(u).animate({width: "100%"}, function() {
-                        c = !0, f = u
-                    })
-                }) : "slide-v" == l.effect && (u >= f ? (l.modul.find("ul li").eq(f).animate({top: -60}), l.modul.find("ul li").eq(u).css({top: 60,display: "block"}), l.modul.find("ul li").eq(u).animate({top: 0}, function() {
-                    f = u, c = !0
-                })) : (l.modul.find("ul li").eq(f).animate({top: 60}), l.modul.find("ul li").eq(u).css({top: -60,display: "block"}), l.modul.find("ul li").eq(u).animate({top: 0}, function() {
-                    f = u, c = !0
-                })))
-            }
-            function t() {
-                for (e = l.feed.split(","), d = l.feedlabels.split(","), s = 0, l.modul.find("ul").html(""), xx = 0, k = 0; k < e.length; k++)
-                    jQuery.ajax({type: "GET",url: document.location.protocol + "//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=" + l.feedcount + "&callback=?&q=" + encodeURIComponent(e[k].trim()),dataType: "json",success: function(n) {
-                            feeddata = n.responseData.feed.entries, jQuery(feeddata).each(function(n) {
-                                s++, l.modul.find("ul").append('<li><a target="_blank" href="' + feeddata[n].link + '"><span>' + d[xx] + "</span> - " + feeddata[n].title + "</a></li>")
-                            }), 0 == xx && l.modul.find("ul li").eq(0).fadeIn(), xx++
-                        },error: function() {
-                            l.modul.find("ul").append("RSS Feed Error!")
-                        }})
-            }
-            l.modul = jQuery("#" + jQuery(this).attr("id"));
-            var a = l.modul, u = 0, f = 0, s = l.modul.find("ul li").length, c = !0;
-            0 != l.feed ? t() : l.modul.find("ul li").eq(u).fadeIn(), n(), l.autoplay ? (a = setInterval(function() {
-                i()
-            }, l.timer), jQuery(l.modul).on("mouseenter", function() {
-                clearInterval(a)
-            }), jQuery(l.modul).on("mouseleave", function() {
-                a = setInterval(function() {
-                    i()
-                }, l.timer)
-            })) : clearInterval(a), l.border || l.modul.addClass("bn-bordernone"), "italic" == l.fontstyle && l.modul.addClass("bn-italic"), "bold" == l.fontstyle && l.modul.addClass("bn-bold"), "bold-italic" == l.fontstyle && l.modul.addClass("bn-bold bn-italic"), l.modul.addClass("bn-" + l.color), jQuery(window).on("resize", function() {
-                n()
-            }), l.modul.find(".bn-navi span").on("click", function() {
-                c && (c = !1, 0 == jQuery(this).index() ? (u--, 0 > u && (u = s - 1), o()) : (u++, u == s && (u = 0), o()))
-            })
-        })
-    }
-}(jQuery);
+(function(jQuery){
+
+    $.fn.eventTicker = function(params){
+            var defaults={
+                    width           :'100%',
+                    modul           :'eventTicker',
+                    color           :'default',
+                    border          :false,
+                    effect          :'fade',
+                    fontstyle       :'normal',
+                    autoplay        :false,
+                    timer           :4000,
+                    feed            :false,
+                    feedlabels      :false,
+                    feedcount       :5
+                };
+            var feeds=[];
+            var labels=[];
+            var params=$.extend(defaults,params);
+
+            return this.each(function(){
+                //Variables------------------------------------
+                params.modul=$("#"+$(this).attr("id"));
+                var timername=params.modul;
+                var active=0;
+                var previous=0;
+                var count=params.modul.find("ul li").length;
+                var changestate=true;
+
+                if (params.feed!=false)
+                {
+                    getRSS();
+                }
+                else
+                {
+                    params.modul.find("ul li").eq(active).fadeIn();
+                }
+                resizeEvent();
+
+                if (params.autoplay)
+                {
+                    timername=setInterval(function(){autoPlay()},params.timer);
+                    $(params.modul).on("mouseenter",function (){
+                        clearInterval(timername);
+                    });
+
+                    $(params.modul).on("mouseleave",function (){
+                        timername=setInterval(function(){autoPlay()},params.timer);
+                    });
+                }
+                else
+                {
+                    clearInterval(timername);
+                }
+
+                if (!params.border)
+                {
+                    params.modul.addClass("bn-bordernone");
+                }
+
+                if (params.fontstyle=="italic")
+                    params.modul.addClass("bn-italic");
+
+                if (params.fontstyle=="bold")
+                    params.modul.addClass("bn-bold");
+
+                if (params.fontstyle=="bold-italic")
+                    params.modul.addClass("bn-bold bn-italic");
+
+                params.modul.addClass("bn-"+params.color);
+
+                //Events---------------------------------------
+                $(window).on("resize",function (){
+                    resizeEvent();
+                });
+
+                params.modul.find(".bn-navi span").on("click",function(){
+                    if (changestate)
+                    {
+                        changestate=false;
+                        if ($(this).index()==0)
+                        {
+                            active--;
+                            if (active<0)
+                                active=count-1;
+
+                            changeNews();
+                        }
+                        else
+                        {
+                            active++;
+                            if (active==count)
+                                active=0;
+
+                            changeNews();
+                        }
+                    }
+                });
+
+                //functions------------------------------------
+                function resizeEvent()
+                {
+                    if (params.modul.width()<480)
+                    {
+                        params.modul.find(".bn-title h2").css({"display":"none"});
+                        params.modul.find(".bn-title").css({"width":10});
+                        params.modul.find("ul").css({"left":30});
+                    }
+                    else
+                    {
+                        params.modul.find(".bn-title h2").css({"display":"inline-block"});
+                        params.modul.find(".bn-title").css({"width":"auto"});
+                        params.modul.find("ul").css({"left":$(params.modul).find(".bn-title").width()+30});
+                    }
+                }
+
+                function autoPlay()
+                {
+                    active++;
+                    if (active==count)
+                        active=0;
+
+                    changeNews();
+                }
+
+                function changeNews()
+                {
+                    if (params.effect=="fade")
+                    {
+                        params.modul.find("ul li").css({"display":"none"});
+                        params.modul.find("ul li").eq(active).fadeIn("normal",function (){
+                            changestate=true;
+                        });
+                    }
+                    else if (params.effect=="slide-h")
+                    {
+                        params.modul.find("ul li").eq(previous).animate({width:0},function(){
+                            $(this).css({"display":"none","width":"100%"});
+                            params.modul.find("ul li").eq(active).css({"width":0,"display":"block"});
+                            params.modul.find("ul li").eq(active).animate({width:"100%"},function(){
+                                changestate=true;
+                                previous=active;
+                            });
+                        });
+                    }
+                    else if (params.effect=="slide-v")
+                    {
+                        if (previous<=active)
+                        {
+                            params.modul.find("ul li").eq(previous).animate({top:-60});
+                            params.modul.find("ul li").eq(active).css({top:60,"display":"block"});
+                            params.modul.find("ul li").eq(active).animate({top:0},function(){
+                                previous=active;
+                                changestate=true;
+                            });
+                        }
+                        else
+                        {
+                            params.modul.find("ul li").eq(previous).animate({top:60});
+                            params.modul.find("ul li").eq(active).css({top:-60,"display":"block"});
+                            params.modul.find("ul li").eq(active).animate({top:0},function(){
+                                previous=active;
+                                changestate=true;
+                            });
+                        }
+                    }
+                }
+
+
+                function getRSSx(a,b,c)
+                {
+                    c=new XMLHttpRequest;
+                    c.open('GET',a);
+                    c.onload=b;
+                    c.send()
+                }
+
+                function yql(a,b)
+                {
+                    return 'http://query.yahooapis.com/v1/public/yql?q='+encodeURIComponent('select * from '+b+' where url=\"'+a+'\" limit '+params.feedcount)+'&format=json';
+                };
+
+                function getRSS()
+                {
+                    feeds=params.feed.split(",");
+                    labels=params.feedlabels.split(",");
+                    count=0;
+                    params.modul.find("ul").html("");
+                    xx=0;
+                    for (k=0; k<feeds.length; k++)
+                    {
+                        getRSSx(yql(feeds[k].trim(),'rss'),function()
+                        {
+                            var resultx=JSON.parse(this.response);
+                            resultx=resultx.query.results.item;
+                            $(resultx).each(function(index, element) {
+                                count++;
+                                dataLink= $('<a>').prop('href', resultx[index].link).prop('hostname');
+                                params.modul.find("ul").append('<li><a target="_blank" href="'+resultx[index].link+'"><span>'+dataLink+'</span> - '+resultx[index].title+'</a></li>');
+                                if (xx==0)
+                                    params.modul.find("ul li").eq(0).fadeIn();
+                                xx++;
+
+                            });
+
+                        })
+                    }
+
+                }
+
+            });
+
+
+        };
+
+})(jQuery);
+
