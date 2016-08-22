2b04302f7609ade9ce6766b567239f8dd97f16e6
Anthony <bousek.antonin@gmail.com>
2016-08-22 14:38:24 +0200

    Cosmetic changes

--- home.php
+++ home.php
@@ -2,7 +2,8 @@
 <div class="container clearfix">
     <!-- TICKER -->
     <div class="eventTicker z-depth-0-half" id="ticker">
-        <div class="bn-title"><h5>Důležité termíny</h5><span></span></div>
+        <div class="bn-title">
+            <h5>Důležité termíny</h5><span></span></div>
         <?php
             // args
             $args = array(
@@ -22,74 +23,84 @@
         // query
         $event = new WP_Query( $args );
         ?>
-        <?php if( $event->have_posts() ): ?>
+            <?php if( $event->have_posts() ): ?>
             <ul>
-            <?php while( $event->have_posts() ) : $event->the_post(); ?>
+                <?php while( $event->have_posts() ) : $event->the_post(); ?>
                 <li class="gacr-event">
                     <?php $date = DateTime::createFromFormat('Ymd', get_field('date')); ?>
                     <span class="date"><?php echo $date->format('j. n. Y'); ?>&nbsp;|</span>
                     <span><a href="#"><?php the_title(); ?></a></span>
                 </li>
-            <?php endwhile; ?>
+                <?php endwhile; ?>
             </ul>
             <div class="bn-navi">
                 <span class="arrow aleft"></span>
                 <span class="arrow aright"></span>
             </div>
             <?php else : ?>
-                <ul>
-                    <li class="gacr-event"><?php _e( 'V nejbližší době nejsou pro řešitele a žadatele žádné důležité termíny.' ); ?></li>
-                </ul>
+            <ul>
+                <li class="gacr-event">
+                    <?php _e( 'V nejbližší době nejsou pro řešitele a žadatele žádné důležité termíny.' ); ?>
+                </li>
+            </ul>
             <?php endif; ?>
-
-        <?php wp_reset_postdata();  // Restore global post data stomped by the_post(). ?>
+            <?php wp_reset_postdata();  // Restore global post data stomped by the_post(). ?>
     </div><!-- END TICKER -->
-        <div class="row">
-            <!-- <div class="col s4">&nbsp;</div> -->
-            <div class="col l9 s12 homepage">
-                <main id="main" class="site-main" role="main"> <!-- NEWS -->
-            <?php
+    <div class="row">
+        <div class="col l9 s12 homepage">
+            <main id="main" class="site-main" role="main">
+                <!-- NEWS -->
+                <?php
                 $custom_args = array (
-                    'cat'                    => '-17, -292, -87',
-                    'posts_per_page'         => '5',
+                    // 'cat'                    => '-17, -292, -87, 53',
+                    'cat' => '331',
+                    'category__not_in' => array(87, 17),  // 87 zverejnene-informace-106; 17 vynikajici projekty;
+                    'posts_per_page' => 5,
+                    'post_status' => 'publish',
                     'paged' => $paged
                 );
 
                 // the query
                 $custom_query = new WP_Query( $custom_args );
-            ?>
 
-                <?php // Output custom query loop
-                    if ( $custom_query->have_posts() ) : ?>
 
-                    <!-- the loop -->
-                    <?php while ( $custom_query->have_posts() ) : $custom_query->the_post(); ?>
+                 // Output custom query loop
+                    if ( $custom_query->have_posts() ) :
 
-                        <?php get_template_part( 'content', 'single' ); ?>
+                    // <!-- the loop -->
+                     while ( $custom_query->have_posts() ) : $custom_query->the_post();
 
-                    <?php endwhile; ?>
+                         get_template_part( 'content', 'single' );
+
+                     endwhile; ?>
                     <!-- end of the loop -->
-
-
-                <?php else : ?>
-                    <p><?php esc_html_e ( 'Nic zde není.' ); ?></p>
-                <?php endif; ?>
-                <?php wp_reset_postdata(); ?>
-            </main> <!-- END NEWS -->
-                <div class="loadmore">
-                    <a id="a2" class="gacr-blue waves-effect waves-light btn-floating btn-large ajaxload" href="/novinky/">
-                        <i class="material-icons">&#xE149;</i>
-                    </a><span>ARCHIV</span>
-                </div>
-            </div> <!-- col homepage -->
-            <?php if ( is_active_sidebar( 'homepage', 'homepage-button' ) ) : ?>
-                <div class="col l3 homepage">
-                    <?php dynamic_sidebar( 'homepage' ); ?>
-                    <?php dynamic_sidebar( 'homepage-button' ); ?>
-                </div>
-                <?php else : ?>
-                    <p><?php _e( 'Nic zde není.' ); ?></p>
-                <?php endif; ?>
-        </div> <!-- END ROW -->
-</div> <!-- END CONTAINER -->
+                    <?php else : ?>
+                    <p>
+                        <?php esc_html_e ( 'Nic zde není.' ); ?>
+                    </p>
+                    <?php endif;
+                 wp_reset_postdata(); ?>
+            </main>
+            <!-- END NEWS -->
+            <div class="loadmore">
+                <a id="a2" class="gacr-blue waves-effect waves-light btn-floating btn-large ajaxload" href="/novinky/">
+                    <i class="material-icons">&#xE149;</i>
+                </a><span>ARCHIV</span>
+            </div>
+        </div>
+        <!-- col homepage -->
+        <?php if ( is_active_sidebar( 'homepage', 'homepage-button' ) ) : ?>
+        <div class="col l3 homepage">
+            <?php dynamic_sidebar( 'homepage' ); ?>
+            <?php dynamic_sidebar( 'homepage-button' ); ?>
+        </div>
+        <?php else : ?>
+        <p>
+            <?php _e( 'Nic zde není.' ); ?>
+        </p>
+        <?php endif; ?>
+    </div>
+    <!-- END ROW -->
+</div>
+<!-- END CONTAINER -->
 <?php get_footer(); ?>
