f149a4ee506eb1a7073d2a9e071d85338c62de03
Anthony <bousek.antonin@gmail.com>
2016-08-26 15:58:30 +0200

    Added dropdown function
    also nested dropdown function with gutter

--- js/imhere.js
+++ js/imhere.js
@@ -14,15 +14,27 @@
          // $('.side-nav').addClass('morph-menu-wrapper-active');
 
          // dropdown initialization
-         // $('.dropdown-button').dropdown({
-         //     inDuration: 300,
-         //     outDuration: 225,
-         //     constrain_width: false, // Does not change width of dropdown to that of the activator
-         //     hover: true, // Activate on hover
-         //     gutter: 0, // Spacing from edge
-         //     belowOrigin: true, // Displays dropdown below the button
-         //     alignment: 'left' // Displays dropdown with edge aligned to the left of button
-         // });
+  $('.dropdown-button.sub-menu-item').dropdown({
+      inDuration: 300,
+      outDuration: 225,
+      constrain_width: false, // Does not change width of dropdown to that of the activator
+      hover: true, // Activate on hover
+      gutter: ($('.dropdown-content').width()*3)/3.05 + 3, // Spacing from edge
+      belowOrigin: false, // Displays dropdown below the button
+      alignment: 'left' // Displays dropdown with edge aligned to the left of button
+    }
+  );
+
+  $('.dropdown-button.main-menu-item').dropdown({
+      inDuration: 300,
+      outDuration: 225,
+      constrain_width: true, // Does not change width of dropdown to that of the activator
+      hover: true, // Activate on hover
+      belowOrigin: true, // Displays dropdown below the button
+      alignment: 'left' // Displays dropdown with edge aligned to the left of button
+    }
+  );
+
 
         //  // MD loding transition
         $('[data-animation="animate"]').onScreen({
