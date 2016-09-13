6b5c4539b93e6994765915814dacecd6f8c78be9
Anthony <bousek.antonin@gmail.com>
2016-09-13 10:35:08 +0200

    changed ordering

--- js/imhere.js
+++ js/imhere.js
@@ -13,27 +13,26 @@
          // $('li.page_item').addClass('waves-effect waves-light');
          // $('.side-nav').addClass('morph-menu-wrapper-active');
 
-         // dropdown initialization
-  $('.dropdown-button.sub-menu-item').dropdown({
-      inDuration: 300,
-      outDuration: 225,
-      constrain_width: false, // Does not change width of dropdown to that of the activator
-      hover: true, // Activate on hover
-      gutter: ($('.dropdown-content').width()*3)/3.05 + 3, // Spacing from edge
-      belowOrigin: false, // Displays dropdown below the button
-      alignment: 'left' // Displays dropdown with edge aligned to the left of button
-    }
-  );
+// main dropdown initialization
+$('.dropdown-button.main-menu-item').dropdown({
+    inDuration: 300,
+    outDuration: 225,
+    constrain_width: true, // Does change width of dropdown to that of the activator
+    hover: true, // Activate on hover
+    belowOrigin: true, // Displays dropdown below the button
+    alignment: 'left' // Displays dropdown with edge aligned to the left of button
+});
+// nested dropdown initialization
+$('.dropdown-button.sub-menu-item').dropdown({
+    inDuration: 300,
+    outDuration: 225,
+    constrain_width: false, // Does not change width of dropdown to that of the activator
+    hover: true, // Activate on hover
+    gutter: ($('.dropdown-content').width() * 3) / 3.05 + 3, // Spacing from edge
+    belowOrigin: false, // Displays dropdown below the button
+    alignment: 'left' // Displays dropdown with edge aligned to the left of button
+});
 
-  $('.dropdown-button.main-menu-item').dropdown({
-      inDuration: 300,
-      outDuration: 225,
-      constrain_width: true, // Does not change width of dropdown to that of the activator
-      hover: true, // Activate on hover
-      belowOrigin: true, // Displays dropdown below the button
-      alignment: 'left' // Displays dropdown with edge aligned to the left of button
-    }
-  );
 
 
         //  // MD loding transition
